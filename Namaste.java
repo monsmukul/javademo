11e2110e2573375afda9138da4c9536827723132
Monika <monika03mukul@gmail.com>
2023-05-29 09:36:37 +0530

    First commit

---
+++ Namaste.java
@@ -0,0 +1,8 @@
+public class Namaste{
+	public static void main(String args[])
+	{
+		System.out.println("Namaste World!");
+		System.out.println("Namaste India!");
+		System.out.println("Namaste Bihar!");
+	}
+}
